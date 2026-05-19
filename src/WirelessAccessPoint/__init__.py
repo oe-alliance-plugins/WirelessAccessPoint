5bc5cd9cf04ef45be5c5d3236905bca6e2f5b0a6
jbleyel <jbleyel@gmx.net>
2026-05-19 16:55:24 +0200

    translation update

--- src/WirelessAccessPoint/__init__.py
+++ src/WirelessAccessPoint/__init__.py
@@ -1,2 +1,23 @@
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+from gettext import bindtextdomain, dgettext, gettext
+
+
+PluginLanguageDomain = "WirelessAccessPoint"
+
+
+def locale_init():
+	bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, "SystemPlugins/WirelessAccessPoint/locale"))
+
+
+def _(txt):
+	t = dgettext(PluginLanguageDomain, txt)
+	if t == txt:
+		t = gettext(txt)
+	return t
+
+
+locale_init()
+language.addCallback(locale_init)
 
 __version__ = "1.0"
